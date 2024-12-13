46305e2a4917d0f078d5abf6d938fafb61867162
siddarth12345 <60466648+siddarth12345@users.noreply.github.com>
2024-12-13 15:10:59 +0530

    Add 'sq' header logic
    @RequestHeader gets the value of'sq' header which is then squared and added to the response map

--- src/main/java/com/example/API_endpoint/EchoController.java
+++ src/main/java/com/example/API_endpoint/EchoController.java
@@ -1,6 +1,7 @@
 package com.example.API_endpoint;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -11,10 +12,23 @@ import java.util.Map;
 public class EchoController {
     //Define endpoint
     @PostMapping("/echo/{customerNumber}")
-    public Map<String, Object> echo() {
+    public Map<String, Object> echo(@RequestHeader Map<String, String> headers) {
         // create response map
         Map<String, Object> response = new HashMap<>();
         response.put("success", true);
+
+        //@RequestHeader gets mapping for header values
+        //check if the 'sq' head exists
+        if (headers.containsKey("sq")) {
+            try {
+                // parse 'sq' as an integer
+                int sq = Integer.parseInt(headers.get("sq"));
+                response.put("sq", sq * sq);
+            } catch (NumberFormatException e) {
+                // handle invalid values
+                response.put("sq", "Enter valid 'sq' values");
+            }
+        }
 
         return response;
     }
